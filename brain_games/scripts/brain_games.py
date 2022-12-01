e1f61b6bc5fa0ca0334861498ede8b6722937a74
losinginterests <mmartzisrea@gmail.com>
2022-12-02 01:02:35 +0300

    step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,5 +2,12 @@
 
 def main():
     print('Welcome to the Brain Games!')
-
 main()
+
+def welcome_user():
+    name = ''
+    while name == '':
+        print(f'May I have your name?', end = ' ')
+        name = input()
+        print(f'Hello, {name}!')
+welcome_user()
